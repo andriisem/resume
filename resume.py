abf533736cf8d0585d019c1b4557432c78a34544
Andrii Semko <semko.andrey.i@gmail.com>
2023-10-16 17:43:15 +0300

    Update resume.py

--- resume.py
+++ resume.py
@@ -3,9 +3,8 @@ import subprocess
 latex_file = 'resume.tex'
 output_pdf_name = 'AndriiSemko'
 
-# Compile the LaTeX document using xelatex with a custom output filename
 try:
     subprocess.run(['xelatex', '-jobname=' + output_pdf_name, latex_file], check=True)
     print(f"PDF generated successfully as {output_pdf_name}!")
 except subprocess.CalledProcessError as e:
-    print("Error generating PDF:", e)
+    print(f"Error generating PDF: {e}")
